ace4a10829a95f0cc3415cf0eb968cd1601287f1
RoopaChiluvuri <roopasree2509@gmail.com>
2024-07-30 20:11:26 -0700

    created UIControl.java

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        var control = new UIControl();
+        control.disable();
+        System.out.println(control.isEnabled());
+    }
+}
