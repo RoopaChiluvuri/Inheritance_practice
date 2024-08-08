aedc327f74733d6634f388d5970802ddaf6b4ed6
RoopaChiluvuri <roopasree2509@gmail.com>
2024-08-07 18:18:35 -0700

    practicing Inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        var control = new UIControl();
+        var control = new TextBox();
         control.disable();
         System.out.println(control.isEnabled());
     }
